cfa89bc2ca3fd89799e13b477688897d76aed7a7
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-09 14:17:17 +0800

    Update Utils.php

--- Utils.php
+++ Utils.php
@@ -52,7 +52,7 @@ class Utils{
 		$h = (int)substr($tz, 1, 2);
 		$m = (int)substr($tz, 3, 4);
 		$secs = 3600*$h + 60*$m;
-		if($tz[0] == '-')$secs = -$secs;
+		if($tz[0] == '-')$secs = -$secs; //note: used square brackets instead of curly as curly is deprecated
 		return $secs;
 	}
 	
