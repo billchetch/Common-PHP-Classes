4c9c45eac7c84df64a8cd0cb3de924042e90f67d
billchetch <45006134+billchetch@users.noreply.github.com>
2022-07-07 11:47:57 +0800

    s

--- Utils.php
+++ Utils.php
@@ -59,4 +59,12 @@ class Utils{
 	static public function isWindows(){
 		return (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN');
 	}
+
+	static public function humanFilesize($bytes, $dec = 2) 
+	{
+    		$size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    		$factor = floor((strlen($bytes) - 1) / 3);
+
+    		return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+	}
 }
